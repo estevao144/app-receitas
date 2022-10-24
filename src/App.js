406cc542fc7b9fa60318d050f32c5dc8c896cf06
Estevao <estevo144@gmail.com>
2022-10-24 17:08:54 -0300

    é isso

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Drinks from './pages/Drinks';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Provider>
+      <Header />
+      <Drinks />
+    </Provider>
   );
 }
 
