2abe926691429509d5818d1a40e9bce16a9811ff
Lucdomingues <lojalucc@outlook.com>
2022-10-24 18:55:33 -0300

    requisito 06

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from './components/Login';
+import Meals from './pages/Meals';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/meals" component={ Meals } />
       </Switch>
     </div>
   );
---
+++ src/pages/Meals.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Meals() {
+  return (
+    <div>
+      <h1> Tela de Receitas</h1>
+    </div>
+  );
+}
+
+export default Meals;
