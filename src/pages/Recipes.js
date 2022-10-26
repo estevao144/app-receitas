1639041fc077f7086d65cb6ed3bfb99a052ecb8d
Leonardo Selegar <leonardo11132@gmail.com>
2022-10-25 21:04:20 -0300

    Req 19 e 20 com a main atualizada

---
+++ src/pages/Recipes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Meals from './Meals';
+import Header from '../components/Header';
+
+function Recipes() {
+  return (
+    <div>
+      <Header />
+      <Meals />
+    </div>
+  );
+}
+
+export default Recipes;
