973039f19dfdf2586b48d7848eb2d5002a45c439
Estevao <estevo144@gmail.com>
2022-10-26 18:57:31 -0300

    troquei login para pages

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import Login from '../components/Login';
+import Login from '../pages/Login';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
